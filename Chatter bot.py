b3f9a9eeed4f730d11dce3bc51b9108066d8d7a6
CarelHernandez <46796722+CarelHernandez@users.noreply.github.com>
2019-02-22 14:22:35 -0700

    Add files via upload

---
+++ Chatter bot.py
@@ -0,0 +1,24 @@
+    print(" hi my name is Car-two-el, I am here to help you get comftorable with talking to people so dont get intrigued. So whats your name?)
+    Name == input ()
+    print (" Hi there" + Name + " so I heard your shy? its okay let me help you get comfortable?" )
+    answer = input ()
+    if answer == "okay" or answer == "fine" answer == "I guess" answer == "ok"
+          print ("okay then lets get started" )
+    elif anser == "no" or answer == "na" or answer == "its okay im fine"      
+          break 
+    print (" what sport do you like?"
+    sport == input()
+    print (" okay, imagine I was playing" + sport + " and you wanted to play with me, how would you approach me?")
+    input ()
+    if input == "idk" or input == "nothing"
+        print (" come on you got this, just think") 
+    print (" ok let try actually having a normal conversation, just relax and breathe." )
+    print (" lets start off again by, What is something cool about you that you think someone would be amazed about?")
+    input ()
+    if input !== "idk"
+           print " that's amazing, yeah share that with someone new you meet! with me I would say that I am a 17 year old robot made by a human to help a human" )
+    print (" would you need anymore help with being comfortable while interacting with humans?" )
+    Hmm = input ()
+    if Hmm == "no thanks" or Hmm == "no" or Hmm == "na" or Hmm == "nope"
+          break
+    print (" I am gonna go to rest, i am over heating, have a good day!") 
